d17fc28497d7be0bb10dbda76937e7bbee28462f
Wijkarn <mathias.wijk@hotmail.com>
2025-06-14 15:57:10 +0200

    Added ZombifiedPiglin xp bug

--- src/main/java/net/wijk/chest/WijkChest.java
+++ src/main/java/net/wijk/chest/WijkChest.java
@@ -6,15 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WijkChest implements ModInitializer {
-	public static final String MOD_ID = "chest";
+    public static final String MOD_ID = "chest";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
-	}
-
-
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initialized " + MOD_ID);
+    }
 }
--- src/main/java/net/wijk/chest/mixin/WijkChestMixin.java
+++ src/main/java/net/wijk/chest/mixin/WijkChestMixin.java
@@ -8,8 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class WijkChestMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+    @Inject(at = @At("HEAD"), method = "loadWorld")
+    private void init(CallbackInfo info) {
+        // This code is injected into the start of MinecraftServer.loadWorld()V
+    }
 }
